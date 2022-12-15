a4209a06c5ec253b51c9a5ca635e7c7d87307d4f
Minji Kim <qwe38280@gmail.com>
2022-12-15 10:57:54 +0900

    서버 설정 변경

--- app-public-api/src/main/java/co/kurrant/app/public_api/config/SwaggerConfig.java
+++ app-public-api/src/main/java/co/kurrant/app/public_api/config/SwaggerConfig.java
@@ -1,15 +1,7 @@
 package co.kurrant.app.public_api.config;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
