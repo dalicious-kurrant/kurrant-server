eb9e7486888c52062fc84a2fb8684d550678874e
“sangjin <sangjin.lee@dalicious.co>
2023-01-12 13:58:22 +0900

    리팩토링 및 장바구니 확인시 할인금액 및 할인율 변경,추가

---
+++ domain-board/src/main/java/co/dalicious/domain/board/repository/QNoticeRepository.java
@@ -0,0 +1,22 @@
+package co.dalicious.domain.board.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
+
+import static co.dalicious.domain.board.entity.QNotice.notice;
+
+@Repository
+@RequiredArgsConstructor
+public class QNoticeRepository {
+
+    public final JPAQueryFactory queryFactory;
+
+
+    public Object findAllByType(PageRequest pageRequest, Integer type) {
+        return queryFactory.selectFrom(notice)
+                .where(notice.Type.eq(type))
+                .fetch();
+    }
+}
