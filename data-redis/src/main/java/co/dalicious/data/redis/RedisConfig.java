e8215276fe0cf08be9ec0cc2ac939d5d5d453031
Minji Kim <qwe38280@gmail.com>
2023-01-19 15:09:15 +0900

    redisConfig 수정

--- data-redis/src/main/java/co/dalicious/data/redis/RedisConfig.java
+++ data-redis/src/main/java/co/dalicious/data/redis/RedisConfig.java
@@ -4,6 +4,8 @@ import co.dalicious.data.redis.entity.CertificationHash;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,17 +13,19 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.List;
+
 @Configuration
 @EnableRedisRepositories
+@PropertySource("classpath:application-redis.yml")
 public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String host;
+    @Value("${spring.redis.cluster.nodes}")
+    private List<String> clusterNodes;
 
-    @Value("${spring.redis.port}")
-    private int port;
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(clusterNodes);
+        return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 
     @Bean
