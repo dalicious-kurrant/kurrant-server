fe840fdb36fa8c691cce315d4607f1a6987d898b
“sangjin <sangjin.lee@dalicious.co>
2023-01-19 10:30:42 +0900

    diningType 자료형 변경

--- app-public-api/src/main/java/co/kurrant/app/public_api/mapper/order/DailyFoodMapper.java
+++ app-public-api/src/main/java/co/kurrant/app/public_api/mapper/order/DailyFoodMapper.java
@@ -1,8 +1,6 @@
 package co.kurrant.app.public_api.mapper.order;
 
-import co.dalicious.client.core.mapper.GenericMapper;
 import co.dalicious.domain.food.entity.DailyFood;
-
 import co.dalicious.domain.food.entity.Food;
 import co.dalicious.system.util.DiningType;
 import co.dalicious.system.util.FoodStatus;
@@ -10,38 +8,36 @@ import co.kurrant.app.public_api.dto.food.DailyFoodDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
-import org.mapstruct.factory.Mappers;
 
 import java.math.BigDecimal;
 
 @Mapper(componentModel = "spring")
-public interface DailyFoodMapper extends GenericMapper<DailyFoodDto, DailyFood> {
+public interface DailyFoodMapper {
 
-      DailyFoodMapper INSTANCE = Mappers.getMapper(DailyFoodMapper.class);
-
-      DailyFood toEntity(DailyFoodDto dto);
-
-      DailyFoodDto toDto(DailyFood dailyFood);
-
-      @Mapping(source = "dailyFood", target = "food", qualifiedByName = "generateFood")
-      @Mapping(source = "diningType", target = "diningType")
+      @Mapping(source = "dailyFood.food", target = "food")
+      @Mapping(source = "diningType", target = "diningType", qualifiedByName = "diningType")
       @Mapping(source = "status", target = "status")
       DailyFoodDto toDailyFoodDto(DailyFood dailyFood,DiningType diningType,
                                   FoodStatus status, Integer discountedPrice, BigDecimal discountRate);
 
 
-      @Named("generateFood")
-      default Food generateFood(DailyFood dailyFood){
-            return Food.builder()
-                    .id(dailyFood.getId())
-                    .name(dailyFood.getFood().getName())
-                    .discountedRate(dailyFood.getFood().getDiscountedRate())
-                    .spicy(dailyFood.getFood().getSpicy())
-                    .price(dailyFood.getFood().getPrice())
-                    .makers(dailyFood.getFood().getMakers())
-                    .img(dailyFood.getFood().getImg())
-                    .description(dailyFood.getFood().getDescription())
-                    .build();
+//      @Named("generateFood")
+//      default Food generateFood(DailyFood dailyFood){
+//            return Food.builder()
+//                    .id(dailyFood.getId())
+//                    .name(dailyFood.getFood().getName())
+//                    .discountedRate(dailyFood.getFood().getDiscountedRate())
+//                    .spicy(dailyFood.getFood().getSpicy())
+//                    .price(dailyFood.getFood().getPrice())
+//                    .makers(dailyFood.getFood().getMakers())
+//                    .img(dailyFood.getFood().getImg())
+//                    .description(dailyFood.getFood().getDescription())
+//                    .build();
+//      }
+
+      @Named("diningType")
+      default Integer diningType(DiningType diningType){
+            return diningType.getCode();
       }
 
 }
