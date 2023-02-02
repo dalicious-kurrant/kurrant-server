8a69fe7ae99a5cfbb11e9ecc7be4157b172c5b50
Minji Kim <qwe38280@gmail.com>
2023-02-02 11:16:45 +0900

    정기식사 카드 수정

--- domain-order/src/main/java/co/dalicious/domain/order/entity/Order.java
+++ domain-order/src/main/java/co/dalicious/domain/order/entity/Order.java
@@ -112,12 +112,13 @@ public class Order {
         this.creditCardInfo = creditCardInfo;
     }
 
-    public Order(String code, OrderType orderType, Address address, PaymentType paymentType, User user, String receiptUrl) {
+    public Order(String code, OrderType orderType, Address address, PaymentType paymentType, User user, String receiptUrl, CreditCardInfo creditCardInfo) {
         this.code = code;
         this.orderType = orderType;
         this.address = address;
         this.paymentType = paymentType;
         this.user = user;
+        this.creditCardInfo = creditCardInfo;
         this.receiptUrl = receiptUrl;
     }
 
