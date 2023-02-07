d8780fe530f91e368720750cc99178d7bddba52e
“sangjin <sangjin.lee@dalicious.co>
2023-02-07 17:46:06 +0900

    결제승인 추가

---
+++ app-public-api/src/main/java/co/kurrant/app/public_api/dto/order/OrderByServiceDateNotyDto.java
@@ -0,0 +1,24 @@
+package co.kurrant.app.public_api.dto.order;
+
+import co.dalicious.system.util.enums.DiningType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderByServiceDateNotyDto {
+    private DiningType type;
+    private List<String> serviceDays;
+    private LocalTime lastOrderTime;
+
+    @Builder
+    public OrderByServiceDateNotyDto(DiningType type, List<String> serviceDays, LocalTime lastOrderTime) {
+        this.type = type;
+        this.lastOrderTime = lastOrderTime;
+        this.serviceDays = serviceDays;
+    }
+}
---
+++ data-redis/src/main/java/co/dalicious/data/redis/entity/NotificationHash.java
@@ -0,0 +1,57 @@
+package co.dalicious.data.redis.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Getter
+@RedisHash(value = "notificationHash")
+public class NotificationHash {
+    @Id
+    String id;
+
+    // 1: 전체공지, 2: 스팟공지, 3: 구매후기, 4: 마감시간, 5: 다음주 주문, 6: 스팟등록
+    @Indexed
+    Integer type;
+
+    @Indexed
+    BigInteger userId;
+
+    @Indexed
+    boolean isRead;
+
+    String content;
+
+    LocalDate createDate;
+
+    @Builder
+    public NotificationHash(String id, Integer type, BigInteger userId, boolean isRead, String content, LocalDate createDate) {
+        this.id = id;
+        this.type = type;
+        this.userId = userId;
+        this.isRead = isRead;
+        checkNotificationContent(content);
+        this.createDate = createDate;
+    }
+
+    private void checkNotificationContent(String content){
+        if(content == null || content.isEmpty()) {
+            this.content = null;
+        } else if(content.length() > 50) {
+            this.content = content.substring(0, 50);
+        } else {
+            this.content = content;
+        }
+    }
+
+    public void updateRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+}
