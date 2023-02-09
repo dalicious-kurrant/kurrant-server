25c95cae8f43c87ce57d686f21ad1f2d0f1ca8d3
“sangjin <sangjin.lee@dalicious.co>
2023-02-09 14:23:56 +0900

    card삭제시 상태값, 빌링키만 변경되도록 수정

--- app-admin-api/src/main/java/co/kurrant/app/admin_api/controller/FileController.java
+++ app-admin-api/src/main/java/co/kurrant/app/admin_api/controller/FileController.java
@@ -1,19 +1,10 @@
 package co.kurrant.app.admin_api.controller;
 
-import javax.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import co.dalicious.domain.file.dto.RequestImageUploadUrlRequestDto;
-import co.dalicious.domain.file.dto.RequestImageUploadUrlResponseDto;
 import co.dalicious.domain.file.service.ImageService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "File")
 @RequiredArgsConstructor
