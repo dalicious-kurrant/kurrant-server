1cf55b0d35055d19ae221a9a44d03c7b47f3420e
“sangjin <sangjin.lee@dalicious.co>
2023-02-17 15:20:31 +0900

    고객사 로그인 추가

--- app-client-api/src/main/java/co/kurrant/app/client_api/service/AuthService.java
+++ app-client-api/src/main/java/co/kurrant/app/client_api/service/AuthService.java
@@ -1,5 +1,8 @@
 package co.kurrant.app.client_api.service;
 
+import co.kurrant.app.client_api.dto.LoginRequestDto;
+import co.kurrant.app.client_api.dto.LoginResponseDto;
+
 public interface AuthService {
-  //public LoginResponseDto login(LoginRequestDto body);
+  public LoginResponseDto login(LoginRequestDto body);
 }
