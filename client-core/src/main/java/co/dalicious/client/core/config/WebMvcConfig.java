ebd0dfb29cf3a2d2ccd35d3d2b1198b04195e300
Minji Kim <qwe38280@gmail.com>
2023-02-20 17:45:33 +0900

    Cors 오류 해결 시도 (2)

--- client-core/src/main/java/co/dalicious/client/core/config/WebMvcConfig.java
+++ client-core/src/main/java/co/dalicious/client/core/config/WebMvcConfig.java
@@ -22,8 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/v1")
-            .allowedOrigins("http://localhost:3000")
+    registry.addMapping("/v1/**")
             .allowedMethods("GET", "POST", "PATCH", "DELETE")
             .allowCredentials(true);
   }
