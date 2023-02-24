ae9445b5ee79fdce21d3f2659b03a01cf61d2721
“sangjin <sangjin.lee@dalicious.co>
2023-02-24 15:53:31 +0900

    오류수정

--- app-makers-api/src/main/java/co/kurrant/app/makers_api/service/impl/MakersInfoServiceImpl.java
+++ app-makers-api/src/main/java/co/kurrant/app/makers_api/service/impl/MakersInfoServiceImpl.java
@@ -1,11 +1,22 @@
 package co.kurrant.app.makers_api.service.impl;
 
+import co.dalicious.domain.food.entity.Makers;
+import co.dalicious.domain.food.repository.QMakersRepository;
 import co.kurrant.app.makers_api.service.MakersInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class MakersInfoServiceImpl implements MakersInfoService {
+
+    private final QMakersRepository qMakersRepository;
 
     @Override
     public Object getMakersInfo(String code) {
+
+        Makers makers = qMakersRepository.findOneByCode(code);
+
         return null;
     }
 }
