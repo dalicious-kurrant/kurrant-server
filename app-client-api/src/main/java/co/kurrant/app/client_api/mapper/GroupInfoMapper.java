dc09d40e5fbd817161bd4996a15c65f66095b1e2
neya0 <wltjd1484@gmail.com>
2023-02-28 15:52:59 +0900

    고객사 기업정보 조회 수정

--- app-client-api/src/main/java/co/kurrant/app/client_api/mapper/GroupInfoMapper.java
+++ app-client-api/src/main/java/co/kurrant/app/client_api/mapper/GroupInfoMapper.java
@@ -31,8 +31,6 @@ public interface GroupInfoMapper {
     @Mapping(source = "group", target = "isSetting", qualifiedByName = "getIsSetting")
     @Mapping(source = "group", target = "isGarbage", qualifiedByName = "getIsGarbage")
     @Mapping(source = "group", target = "isHotStorage", qualifiedByName = "getIsHotStorage")
-    @Mapping(target = "createdDateTime", expression = "java(DateUtils.toISO(group.getCreatedDateTime()))")
-    @Mapping(target = "updatedDateTime", expression = "java(DateUtils.toISO(group.getUpdatedDateTime()))")
     GroupListDto.GroupInfoList toCorporationListDto(Group group, User managerUser);
 
     @Named("getDiningCodeList")
