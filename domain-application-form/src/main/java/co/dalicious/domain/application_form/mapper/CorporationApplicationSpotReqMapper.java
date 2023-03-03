c1d517aa302da1074c45e3c98cb3012b6fe28eb3
“sangjin <sangjin.lee@dalicious.co>
2023-03-03 13:43:43 +0900

    백오피스 메이커스 수정

--- domain-application-form/src/main/java/co/dalicious/domain/application_form/mapper/CorporationApplicationSpotReqMapper.java
+++ domain-application-form/src/main/java/co/dalicious/domain/application_form/mapper/CorporationApplicationSpotReqMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public interface CorporationApplicationSpotReqMapper {
     @Mapping(source = "spotName", target = "name")
     @Mapping(source = "address", target = "address")
     @Mapping(source = "diningTypes", target = "diningTypes", qualifiedByName = "codeToDiningType")
-    CorporationApplicationFormSpot toEntity(CorporationSpotRequestDto corporationApplicationFormSpot);
+    CorporationApplicationFormSpot toEntity(CorporationSpotRequestDto corporationApplicationFormSpot) throws ParseException;
 
     @Named("codeToDiningType")
     default List<DiningType> codeToDiningType(List<Integer> diningTypes) {
