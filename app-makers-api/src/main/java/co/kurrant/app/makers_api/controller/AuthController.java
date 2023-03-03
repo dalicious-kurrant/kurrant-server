b70ac068a038d93b065c3bb1f4c777a39efd6f86
neya0 <wltjd1484@gmail.com>
2023-03-03 20:30:47 +0900

    클라이언트와 메이커스 토큰 발급 과정 중 오류 수정

--- app-makers-api/src/main/java/co/kurrant/app/makers_api/controller/AuthController.java
+++ app-makers-api/src/main/java/co/kurrant/app/makers_api/controller/AuthController.java
@@ -29,21 +29,21 @@ public class AuthController {
                 .build();
     }
 
-    @Operation(summary = "토큰 재발급", description = "토큰을 재발급한다.")
-    @PostMapping("/reissue")
-    public ResponseMessage reissue(@RequestBody TokenDto dto) {
-        return ResponseMessage.builder()
-                .message("토큰 재발급에 성공하였습니다.")
-                .data(makersService.reissue(dto))
-                .build();
-    }
-
-    @Operation(summary = "로그아웃", description = "로그아웃을 수행한다.")
-    @PostMapping("/logout")
-    public ResponseMessage logout(@RequestBody TokenDto dto) {
-        makersService.logout(dto);
-        return ResponseMessage.builder()
-                .message("로그아웃 되었습니다.")
-                .build();
-    }
+//    @Operation(summary = "토큰 재발급", description = "토큰을 재발급한다.")
+//    @PostMapping("/reissue")
+//    public ResponseMessage reissue(@RequestBody TokenDto dto) {
+//        return ResponseMessage.builder()
+//                .message("토큰 재발급에 성공하였습니다.")
+//                .data(makersService.reissue(dto))
+//                .build();
+//    }
+//
+//    @Operation(summary = "로그아웃", description = "로그아웃을 수행한다.")
+//    @PostMapping("/logout")
+//    public ResponseMessage logout(@RequestBody TokenDto dto) {
+//        makersService.logout(dto);
+//        return ResponseMessage.builder()
+//                .message("로그아웃 되었습니다.")
+//                .build();
+//    }
 }
