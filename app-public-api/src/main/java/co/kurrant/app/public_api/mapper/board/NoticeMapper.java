c50a5d2b30861945f5ea945280b6c25429b5f807
“sangjin <sangjin.lee@dalicious.co>
2023-03-06 14:01:22 +0900

    공지 타입설명 및 시간 양식 수정

--- app-public-api/src/main/java/co/kurrant/app/public_api/mapper/board/NoticeMapper.java
+++ app-public-api/src/main/java/co/kurrant/app/public_api/mapper/board/NoticeMapper.java
@@ -1,12 +1,23 @@
 package co.kurrant.app.public_api.mapper.board;
 
 import co.dalicious.domain.board.entity.Notice;
+import co.dalicious.system.util.DateUtils;
 import co.kurrant.app.public_api.dto.board.NoticeDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.sql.Timestamp;
 
 @Mapper(componentModel = "spring")
 public interface NoticeMapper{
-
+    @Mapping(source = "notice.createdDateTime", target = "created", qualifiedByName = "timeFormat")
+    @Mapping(source = "notice.updatedDateTime", target = "updated", qualifiedByName = "timeFormat")
     NoticeDto toDto(Notice notice);
+
+    @Named("timeFormat")
+    default String timeFormat(Timestamp date){
+        return DateUtils.format(date, "yyyy-MM-dd");
+    }
 
 }
