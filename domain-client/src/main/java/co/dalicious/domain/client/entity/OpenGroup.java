ca12b407aa2890bf12069eb49a550db32fa4c851
neya0 <wltjd1484@gmail.com>
2023-03-07 10:41:52 +0900

    백오피스 상품 수정 요청 dto 수정 및 그룹 전체조회와 오픈그룹 인원 수 추가

--- domain-client/src/main/java/co/dalicious/domain/client/entity/OpenGroup.java
+++ domain-client/src/main/java/co/dalicious/domain/client/entity/OpenGroup.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -22,12 +23,18 @@ import java.util.List;
 @Getter
 @Table(name = "client__open_group")
 public class OpenGroup extends Group{
+
+    @Comment("오픈 그룹 사용자 수")
+    private Integer openGroupUserCount;
+
     @Builder
-    public OpenGroup(Address address, List<DiningType> diningTypes, String name, BigInteger managerId, Integer familyCount) {
+    public OpenGroup(Address address, List<DiningType> diningTypes, String name, BigInteger managerId, Integer openGroupUserCount) {
         super(address, diningTypes, name, managerId);
+        this.openGroupUserCount = openGroupUserCount;
     }
 
     public void updateOpenSpot(GroupExcelRequestDto groupInfoList, Address address, List<DiningType> diningTypeList) {
         updateGroup(address, diningTypeList, groupInfoList.getName(), groupInfoList.getManagerId());
+        this.openGroupUserCount = groupInfoList.getEmployeeCount();
     }
 }
