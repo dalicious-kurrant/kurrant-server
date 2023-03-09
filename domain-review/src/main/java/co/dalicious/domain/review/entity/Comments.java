5432b02b78e964137da6d661951442af1c8b9b73
neya0 <wltjd1484@gmail.com>
2023-03-09 17:35:05 +0900

    백오피스 코멘트 테이블 설계

---
+++ domain-review/src/main/java/co/dalicious/domain/review/entity/Comments.java
@@ -0,0 +1,58 @@
+package co.dalicious.domain.review.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "review__comment")
+public class Comments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT UNSIGNED", nullable = false)
+    @Comment("댓글 PK")
+    private BigInteger id;
+
+    @Column(name = "content" ,nullable = false)
+    @Comment("댓글 내용-최소 10자 이상")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "review_id")
+    @Comment("리뷰 ID")
+    private Reviews reviews;
+
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Column(name = "created_datetime", nullable = false,
+            columnDefinition = "TIMESTAMP(6) DEFAULT NOW(6)")
+    @Comment("생성일")
+    private Timestamp createdDateTime;
+
+    @UpdateTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Column(name = "updated_datetime",
+            columnDefinition = "TIMESTAMP(6) DEFAULT NOW(6)")
+    @Comment("수정일")
+    private Timestamp updatedDateTime;
+
+    @Builder
+    public Comments(String content, Reviews reviews) {
+        this.content = content;
+        this.reviews = reviews;
+    }
+
+}
