e0ff3cd428472ff4ef99e9759eeddb913b9bb2d5
neya0 <wltjd1484@gmail.com>
2023-03-10 16:02:56 +0900

    백오피스 리뷰

--- app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/ReviewServiceImpl.java
+++ app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/ReviewServiceImpl.java
@@ -3,20 +3,34 @@ package co.kurrant.app.admin_api.service.impl;
 import co.dalicious.client.core.dto.request.OffsetBasedPageRequest;
 import co.dalicious.client.core.dto.response.ItemPageableResponseDto;
 import co.dalicious.domain.review.dto.ReviewAdminResDto;
+import co.dalicious.domain.review.entity.Reviews;
+import co.dalicious.domain.review.repository.QReviewRepository;
 import co.kurrant.app.admin_api.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
+    private final QReviewRepository qReviewRepository;
     @Override
     public ItemPageableResponseDto<ReviewAdminResDto> getAllReviews(Map<String, Object> parameters, Integer limit, Integer page, OffsetBasedPageRequest pageable, LocalDate start, LocalDate end) {
-//        BigInteger makersId = parameters.containsKey("makersId")
+        BigInteger makersId = !parameters.containsKey("makersId") || parameters.get("makersId") == null ? null : BigInteger.valueOf(Integer.parseInt(String.valueOf(parameters.get("makersId"))));
+        BigInteger orderItemId = !parameters.containsKey("orderItemId") || parameters.get("orderItemId") == null ? null : BigInteger.valueOf(Integer.parseInt(String.valueOf(parameters.get("orderItemId"))));
+        String foodName = !parameters.containsKey("foodName") || parameters.get("foodName") == null ? null : String.valueOf(parameters.get("foodName"));
+        String writer = !parameters.containsKey("writer") || parameters.get("writer") == null ? null : String.valueOf(parameters.get("writer"));
+        Boolean isMakersComment = !parameters.containsKey("isMakersComment") || parameters.get("isMakersComment") == null ? null : Boolean.valueOf(String.valueOf(parameters.get("isMakersComment")));
+        Boolean isAdminComment = !parameters.containsKey("isAdminComment") || parameters.get("isAdminComment") == null ? null : Boolean.valueOf(String.valueOf(parameters.get("isAdminComment")));
+        Boolean isReport = !parameters.containsKey("isReport") || parameters.get("isReport") == null ? null : Boolean.valueOf(String.valueOf(parameters.get("isReport")));
+
+
+
+        List<Reviews> reviewsList =
         return null;
     }
 }
--- domain-review/src/main/java/co/dalicious/domain/review/repository/QReviewRepository.java
+++ domain-review/src/main/java/co/dalicious/domain/review/repository/QReviewRepository.java
@@ -1,13 +1,23 @@
 package co.dalicious.domain.review.repository;
 
+import co.dalicious.domain.food.entity.Food;
+import co.dalicious.domain.food.entity.Makers;
 import co.dalicious.domain.order.entity.OrderItem;
+import co.dalicious.domain.order.entity.OrderItemDailyFood;
+import co.dalicious.domain.order.entity.QOrderItemDailyFood;
 import co.dalicious.domain.review.entity.Reviews;
 import co.dalicious.domain.user.entity.User;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.List;
 
 import static co.dalicious.domain.review.entity.QReviews.reviews;
 
@@ -16,6 +26,7 @@ import static co.dalicious.domain.review.entity.QReviews.reviews;
 public class QReviewRepository {
 
     public final JPAQueryFactory queryFactory;
+    public final EntityManager entityManager;
 
     public Reviews findByUserAndOrderItem(User user, OrderItem orderItem) {
         return queryFactory
@@ -29,5 +40,36 @@ public class QReviewRepository {
         return queryFactory.selectFrom(reviews)
                 .where(reviews.id.eq(reviewId), reviews.user.eq(user))
                 .fetchOne();
+    }
+
+    public List<Reviews> findAllByFilter(Makers makers, Food food, User user, LocalDate start, LocalDate end) {
+        BooleanBuilder filter = new BooleanBuilder();
+
+        if(makers != null) {
+            filter.and(reviews.food.makers.eq(makers));
+        }
+        if(food != null) {
+            filter.and(reviews.food.eq(food));
+        }
+        if(user != null) {
+            filter.and(reviews.user.eq(user));
+        }
+
+        QOrderItemDailyFood orderItemDailyFood = QOrderItemDailyFood.orderItemDailyFood;
+        JPAQuery<OrderItemDailyFood> query = new JPAQuery<>(entityManager);
+
+        List<OrderItemDailyFood> result = query
+                .select(orderItemDailyFood)
+                .from(orderItemDailyFood)
+                .where(orderItemDailyFood.dailyFood.serviceDate.between(start, end))
+                .fetch();
+
+        return queryFactory.selectFrom(reviews)
+                .where(reviews.orderItem.in(result), filter)
+                .fetch();
+
+
+
+
     }
 }
