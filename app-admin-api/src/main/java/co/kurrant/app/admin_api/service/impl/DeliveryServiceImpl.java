a15d5a43f16682b16be73f27112637f954a39c00
neya0 <wltjd1484@gmail.com>
2023-03-14 15:23:09 +0900

    백오피스 배송 정보 페이지 조회 데이터 픽업 타임 기준 정렬

--- app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/DeliveryServiceImpl.java
+++ app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/DeliveryServiceImpl.java
@@ -79,7 +79,8 @@ public class DeliveryServiceImpl implements DeliveryService {
 
         MultiValueMap<LocalDate, DeliveryDto.DeliveryGroup> deliveryGroupMap = new LinkedMultiValueMap<>();
         for(DeliveryDto.GroupGrouping groupGrouping : deliveryMakersMap.keySet()) {
-            List<DeliveryDto.DeliveryMakers> deliveryMakersList = deliveryMakersMap.get(groupGrouping);
+            List<DeliveryDto.DeliveryMakers> deliveryMakersList = Objects.requireNonNull(deliveryMakersMap.get(groupGrouping))
+                    .stream().sorted(Comparator.comparing(deliveryMakers -> LocalTime.parse(deliveryMakers.getPickupTime()))).collect(Collectors.toList());
 
             DiningType diningType = dailyFoodSet.stream()
                     .filter(v -> v.getGroup().equals(groupGrouping.getGroup()) && v.getServiceDate().equals(groupGrouping.getServiceDate()) && v.getDiningType().equals(groupGrouping.getDiningType()))
