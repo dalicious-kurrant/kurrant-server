eddf13dbb606a407431a3624149a5b5ca127282d
Minji Kim <qwe38280@gmail.com>
2023-03-16 16:55:45 +0900

    메이커스 상품 정렬 요청사항

--- domain-makers/src/main/java/co/dalicious/domain/food/entity/enums/DailyFoodStatus.java
+++ domain-makers/src/main/java/co/dalicious/domain/food/entity/enums/DailyFoodStatus.java
@@ -7,12 +7,13 @@ import java.util.Arrays;
 @Getter
 public enum DailyFoodStatus {
 
-    WAITING("판매대기", 0),
+    WAITING_SALE("판매대기", 0),
     SALES("판매중", 1),
     SOLD_OUT("품절", 2),
     NOT_ALLOW_CANCEL("취소불가품", 3),
     STOP_SALE("판매중지", 4),
-    PASS_LAST_ORDER_TIME("주문마감", 5);
+    WAITING_REGISTER("등록대기", 5),
+    PASS_LAST_ORDER_TIME("주문마감", 6);
 
     private final String status;
     private final Integer code;
