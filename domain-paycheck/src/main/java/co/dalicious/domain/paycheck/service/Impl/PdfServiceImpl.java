13bc80089c58fb3b4eae3d24f12ffcae108f78f0
Minji Kim <qwe38280@gmail.com>
2023-04-12 14:22:51 +0900

    수정

--- domain-paycheck/src/main/java/co/dalicious/domain/paycheck/service/Impl/PdfServiceImpl.java
+++ domain-paycheck/src/main/java/co/dalicious/domain/paycheck/service/Impl/PdfServiceImpl.java
@@ -2,15 +2,11 @@ package co.dalicious.domain.paycheck.service.Impl;
 
 import co.dalicious.domain.paycheck.service.PdfService;
 import lombok.RequiredArgsConstructor;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Service;
-
-import java.awt.image.BufferedImage;
 
 
 @Service
 @RequiredArgsConstructor
-public class PdfServiceImpl {
+public class PdfServiceImpl implements PdfService {
 
 }
--- domain-paycheck/src/main/java/co/dalicious/domain/paycheck/service/PdfService.java
+++ domain-paycheck/src/main/java/co/dalicious/domain/paycheck/service/PdfService.java
@@ -1,8 +1,5 @@
 package co.dalicious.domain.paycheck.service;
 
-import org.apache.poi.ss.usermodel.Workbook;
-
-import java.io.ByteArrayOutputStream;
 
 public interface PdfService {
 }
