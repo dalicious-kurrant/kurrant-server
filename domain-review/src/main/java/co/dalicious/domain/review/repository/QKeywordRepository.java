800ec8066771418d244a0b8598547c8b3bd21bc5
sangjin <sangjin.lee@dalicious.co>
2023-05-10 09:12:27 +0900

    백오피스 상품 리뷰 키워드 추가 기능

---
+++ domain-review/src/main/java/co/dalicious/domain/review/repository/QKeywordRepository.java
@@ -0,0 +1,15 @@
+package co.dalicious.domain.review.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class QKeywordRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+
+
+}
