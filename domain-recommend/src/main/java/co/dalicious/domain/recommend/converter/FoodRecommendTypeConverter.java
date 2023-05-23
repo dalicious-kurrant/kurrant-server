ee26fb367a2f83d61179d2c49c36076e0db0da59
Minji Kim <qwe38280@gmail.com>
2023-05-23 10:07:13 +0900

    Update FoodRecommendTypeConverter.java

--- domain-recommend/src/main/java/co/dalicious/domain/recommend/converter/FoodRecommendTypeConverter.java
+++ domain-recommend/src/main/java/co/dalicious/domain/recommend/converter/FoodRecommendTypeConverter.java
@@ -1,18 +1,15 @@
 package co.dalicious.domain.recommend.converter;
 
 import co.dalicious.domain.recommend.entity.FoodRecommendType;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import co.dalicious.system.enums.FoodTag;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
 
 @Converter
 public class FoodRecommendTypeConverter implements AttributeConverter<List<FoodRecommendType>, String> {
-    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public String convertToDatabaseColumn(List<FoodRecommendType> attribute) {
@@ -20,24 +17,35 @@ public class FoodRecommendTypeConverter implements AttributeConverter<List<FoodR
             return null;
         }
 
-        try {
-            return mapper.writeValueAsString(attribute);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to convert List<FoodRecommendType> to String", e);
+        StringBuilder sb = new StringBuilder();
+        for (FoodRecommendType frt : attribute) {
+            sb.append("(")
+                    .append(frt.getFoodTag().getCode())
+                    .append(",")
+                    .append(frt.getImportance())
+                    .append("),");
         }
+
+        sb.setLength(sb.length() - 1);  // to remove the last comma
+        return sb.toString();
     }
 
     @Override
     public List<FoodRecommendType> convertToEntityAttribute(String dbData) {
-        if (dbData == null || dbData.trim().length() == 0) {
+        if (dbData == null || dbData.trim().isEmpty()) {
             return null;
         }
 
-        try {
-            return mapper.readValue(dbData, new TypeReference<List<FoodRecommendType>>() {
-            });
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String[] entries = dbData.split("\\),\\(");
+        List<FoodRecommendType> resultList = new ArrayList<>();
+        for (String entry : entries) {
+            String cleanEntry = entry.replace("(", "").replace(")", ""); // Remove parentheses
+            String[] parts = cleanEntry.split(",");
+            FoodTag foodTag = FoodTag.ofCode(Integer.valueOf(parts[0].trim()));  // Adjust this according to your FoodTag constructor
+            int importance = Integer.parseInt(parts[1].trim());
+            resultList.add(new FoodRecommendType(foodTag, importance));
         }
+
+        return resultList;
     }
 }
