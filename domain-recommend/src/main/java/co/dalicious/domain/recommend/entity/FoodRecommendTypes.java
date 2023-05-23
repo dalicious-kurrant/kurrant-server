93cc46b2a8ba2ce40a1aabed5d18f1c6a7c7f969
Minji Kim <qwe38280@gmail.com>
2023-05-23 14:57:32 +0900

    수정사항 반영

--- domain-recommend/src/main/java/co/dalicious/domain/recommend/entity/FoodRecommendTypes.java
+++ domain-recommend/src/main/java/co/dalicious/domain/recommend/entity/FoodRecommendTypes.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Embeddable
 public class FoodRecommendTypes {
     @Comment("식품 타입 번호")
+    @Column(name = "orderNumber")
     private Integer order;
 
     @Convert(converter = FoodRecommendTypeConverter.class)
