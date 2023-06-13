ce1d48eb418e787fa6256a93cccf83dadac39ce4
gangjiseong <jisung.kang@dalicioius.co>
2023-06-13 15:09:47 +0900

    신규앱 마이스팟 신청 수정

---
+++ domain-application-form/src/main/java/co/dalicious/domain/application_form/repository/QRequestedMySpotRepository.java
@@ -0,0 +1,22 @@
+package co.dalicious.domain.application_form.repository;
+
+import co.dalicious.domain.application_form.entity.RequestedMySpot;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+
+import static co.dalicious.domain.application_form.entity.QRequestedMySpot.requestedMySpot;
+
+@Repository
+@RequiredArgsConstructor
+public class QRequestedMySpotRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public RequestedMySpot findRequestedMySpotByUserId (BigInteger userId) {
+        return queryFactory.selectFrom(requestedMySpot)
+                .where(requestedMySpot.userId.eq(userId))
+                .fetchOne();
+    }
+}
