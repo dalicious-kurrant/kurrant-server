f6989c764328464363e8839f39a47b07c53be20e
gangjiseong <jisung.kang@dalicioius.co>
2023-06-20 09:03:53 +0900

    마이스팟 이름 수정

--- domain-address/src/main/java/co/dalicious/domain/address/entity/embeddable/Address.java
+++ domain-address/src/main/java/co/dalicious/domain/address/entity/embeddable/Address.java
@@ -74,6 +74,9 @@ public class Address {
     }
 
     public String addressToString() {
+        if (this.address2 == null) {
+            return this.address1;
+        }
         return this.address1 + " " + this.address2;
     }
 
