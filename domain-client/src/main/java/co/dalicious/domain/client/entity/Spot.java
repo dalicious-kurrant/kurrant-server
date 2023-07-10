c84be7661c74469d91db9116c7d33e6795668fd3
neya0 <wltjd1484@gmail.com>
2023-07-10 15:59:56 +0900

    백오피스 유저 탈퇴 및 상세스팟 이름 및 식단 상품 수량 변경 수정

--- domain-client/src/main/java/co/dalicious/domain/client/entity/Spot.java
+++ domain-client/src/main/java/co/dalicious/domain/client/entity/Spot.java
@@ -81,8 +81,7 @@ public class Spot {
     @Comment("스팟 상태 ( 0: 비활성, 1: 활성 )")
     private SpotStatus status = SpotStatus.ACTIVE;
 
-    @Lob
-    @Column(name = "memo")
+    @Column(name = "memo", columnDefinition = "TEXT")
     private String memo;
 
 
@@ -182,4 +181,10 @@ public class Spot {
     }
 
     public void updateSpotStatus(SpotStatus status) { this.status = status; }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    public void updateName(String name) { this.name = name; }
 }
