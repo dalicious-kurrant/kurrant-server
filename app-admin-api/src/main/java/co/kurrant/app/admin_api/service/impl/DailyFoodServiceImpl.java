953dce4c6ed1e1f32070370e9605b279335d73d3
gangjiseong <jisung.kang@dalicioius.co>
2023-07-17 18:35:07 +0900

    백오피스 식단 수정

--- app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/DailyFoodServiceImpl.java
+++ app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/DailyFoodServiceImpl.java
@@ -282,6 +282,9 @@ public class DailyFoodServiceImpl implements DailyFoodService {
             if (deliverySchedule != null && !Objects.equals(pickUpTime, deliverySchedule.getPickupTime())) {
                 dailyFood.getDailyFoodGroup().updatePickupTime(DateUtils.stringToLocalTime(dailyFoodDto.getMakersPickupTime()), DateUtils.stringToLocalTime(dailyFoodDto.getDeliveryTime()));
             }
+            else {
+                dailyFood.getDailyFoodGroup().updateDeliverySchedules(new DeliverySchedule(DateUtils.stringToLocalTime(dailyFoodDto.getDeliveryTime()), DateUtils.stringToLocalTime(dailyFoodDto.getMakersPickupTime())));
+            }
 
             Food food = Food.getFood(updateFoods, dailyFoodDto.getMakersName(), dailyFoodDto.getFoodName());
             FoodCapacity foodCapacity = food.getFoodCapacity(DiningType.ofCode(dailyFoodDto.getDiningType()));
