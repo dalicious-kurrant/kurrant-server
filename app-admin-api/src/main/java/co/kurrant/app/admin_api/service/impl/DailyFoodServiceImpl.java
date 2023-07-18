f4c556b023ca637b22838db305230eaea74c4eac
gangjiseong <jisung.kang@dalicioius.co>
2023-07-18 09:40:59 +0900

    백오피스 식단 현황 예외 추가

--- app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/DailyFoodServiceImpl.java
+++ app-admin-api/src/main/java/co/kurrant/app/admin_api/service/impl/DailyFoodServiceImpl.java
@@ -266,7 +266,7 @@ public class DailyFoodServiceImpl implements DailyFoodService {
             if (deliverySchedule != null && !Objects.equals(DateUtils.stringToLocalTime(dailyFoodDto.getMakersPickupTime()), deliverySchedule.getPickupTime())) {
                 dailyFood.getDailyFoodGroup().updatePickupTime(DateUtils.stringToLocalTime(dailyFoodDto.getMakersPickupTime()), DateUtils.stringToLocalTime(dailyFoodDto.getDeliveryTime()));
             }
-            else {
+            else if(deliverySchedule == null) {
                 dailyFood.getDailyFoodGroup().updateDeliverySchedules(new DeliverySchedule(DateUtils.stringToLocalTime(dailyFoodDto.getDeliveryTime()), DateUtils.stringToLocalTime(dailyFoodDto.getMakersPickupTime())));
             }
 
