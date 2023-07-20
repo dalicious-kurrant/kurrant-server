0995245c58b08534047b595ee4d17c9e7c9ca061
gangjiseong <jisung.kang@dalicioius.co>
2023-07-20 12:49:20 +0900

    배치 리뷰 마감 알림

--- batch/src/main/java/co/kurrant/batch/quartz/QuartzService.java
+++ batch/src/main/java/co/kurrant/batch/quartz/QuartzService.java
@@ -49,10 +49,10 @@ public class QuartzService {
             addJob(QuartzBatchJob.class, "dailyFoodJob2", "메이커스 마감: DailyFood 상태 업데이트 Job", jobParameters, quartzSchedule.getMakersAndFoodLastOrderTimeCron());
             addJob(QuartzBatchJob.class, "orderStatusToDeliveringJob", "배송중으로 상태 업테이트 Job", jobParameters, quartzSchedule.getDeliveryTimeCron());
             addJob(QuartzBatchJob.class, "orderStatusToDeliveredJob", "배송완료로 상태 업테이트 Job", jobParameters, "0 0 23 * * ?");
-            addJob(QuartzBatchJob.class, "reviewJob1", "review 마감시간 푸시알림 Job", jobParameters, "0 0/10 11 * * ?");
             addJob(QuartzBatchJob.class, "userWithdrawalJob1", "User 탈퇴 Job", jobParameters, "0 0 3 * * ?");
             addJob(QuartzBatchJob.class, "refreshTokenJob1", "Refresh Token 삭제 Job", jobParameters, "0 0 4 * * ?");
             addJob(QuartzBatchJob.class, "membershipPayJob1", "Membership 결제 Job", jobParameters, "0 0 6 * * ?");
+            addJob(QuartzBatchJob.class, "reviewJob1", "review 마감시간 푸시알림 Job", jobParameters, quartzSchedule.getDeliveryTimeCron());
             addJob(QuartzBatchJob.class, "pushAlarmJob1", "상품 주문 마감 푸시알림 Job", jobParameters, quartzSchedule.getGroupAndMakersAndFoodLastOrderTimeCron());
             addJob(QuartzBatchJob.class, "pushAlarmJob2", "my spot zone 오픈 푸시알림 Job", jobParameters, "0 5 0 * * ?");
             addJob(QuartzBatchJob.class, "pushAlarmJob3", "my spot zone 중지 푸시알림 Job", jobParameters, "0 0 0 * * ?");
