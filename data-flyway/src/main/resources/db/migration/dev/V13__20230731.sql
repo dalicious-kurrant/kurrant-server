b50158400f377e515c83d82278304b9515cc79bc
gangjiseong <jisung.kang@dalicioius.co>
2023-07-31 16:31:13 +0900

    flyway 빌드 수정

--- data-flyway/src/main/resources/db/migration/dev/V13__20230731.sql
+++ data-flyway/src/main/resources/db/migration/dev/V13__20230731.sql
@@ -13,15 +13,3 @@ create table board__back_office_notice
     is_alarm_talk     tinyint(1)   default 0                    not null comment '상태 0:비활성 / 1:활성',
     CONSTRAINT pk_board__back_office_notice PRIMARY KEY (id)
 );
-
-create table board__images
-(
-    back_office_notice_id           bigint unsigned                           not null,
-    img_created_datetime timestamp(6) default CURRENT_TIMESTAMP(6) null comment '생성일',
-    filename             varchar(1024)                             null comment '파일명, S3최대값',
-    s3_key               varchar(1024)                             null comment 'S3 업로드 키',
-    file_location        varchar(2048)                             null comment 'S3 접근 위치',
-    constraint FK2rhrp9fjsca6gi6agyg5w3r98
-        foreign key (back_office_notice_id) references board__back_office_notice (id)
-)
-    charset = utf8mb4;
