2b5cb0cbdb2033112a8865f1a421ed50b13a1309
sangjin <sangjin.lee@dalicious.co>
2023-08-01 16:25:37 +0900

    앱 - 회원가입 안되는 현상 수정(부서 제거)

--- domain-user/src/main/java/co/dalicious/domain/user/mapper/UserDepartmentMapper.java
+++ domain-user/src/main/java/co/dalicious/domain/user/mapper/UserDepartmentMapper.java
@@ -2,11 +2,8 @@ package co.dalicious.domain.user.mapper;
 
 import co.dalicious.domain.client.entity.Department;
 import co.dalicious.domain.user.entity.User;
-import co.dalicious.domain.user.entity.UserDepartment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
-import java.math.BigInteger;
 
 @Mapper(componentModel = "spring")
 public interface UserDepartmentMapper {
--- domain-user/src/main/java/co/dalicious/domain/user/repository/UserDepartmentRepository.java
+++ domain-user/src/main/java/co/dalicious/domain/user/repository/UserDepartmentRepository.java
@@ -1,6 +1,5 @@
 package co.dalicious.domain.user.repository;
 
-import co.dalicious.domain.user.entity.UserDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigInteger;
