4968130bb310637d4c4aabdaf6b172a398c9f613
sangjin <sangjin.lee@dalicious.co>
2023-08-08 16:41:08 +0900

    앱 - 상품상세리뷰 리뷰작성버튼수정3

--- domain-order/src/main/java/co/dalicious/domain/order/repository/QOrderItemDailyFoodRepository.java
+++ domain-order/src/main/java/co/dalicious/domain/order/repository/QOrderItemDailyFoodRepository.java
@@ -1,24 +1,19 @@
 package co.dalicious.domain.order.repository;
 
-import co.dalicious.domain.order.entity.Order;
 import co.dalicious.domain.order.entity.OrderItemDailyFood;
 import co.dalicious.domain.order.entity.QOrderItemDailyFood;
 import co.dalicious.domain.order.entity.enums.OrderStatus;
 import co.dalicious.domain.user.entity.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import net.bytebuddy.asm.Advice;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.List;
 
 import static co.dalicious.domain.food.entity.QDailyFood.dailyFood;
 import static co.dalicious.domain.order.entity.QOrderItemDailyFood.orderItemDailyFood;
-import static co.dalicious.domain.order.entity.QOrder.order;
 
 @Repository
 @RequiredArgsConstructor
@@ -28,23 +23,17 @@ public class QOrderItemDailyFoodRepository {
 
     public OrderItemDailyFood findAllByUserAndDailyFood(BigInteger userId, BigInteger dailyFoodId) {
 
-        /*
-        * userId로 주문내역을 조회(order)
-        * order중에 serviceDay가 5일 이내이면서
-        * foodId가 일치하는것 조회
-        * */
+        /* userId로 주문내역을 조회(order)
+        *  order중에 serviceDay가 5일 이내이면서
+        *  foodId가 일치하는것 조회*/
+
         System.out.println(dailyFoodId + "dailyFoodId");
-        OrderItemDailyFood orderItemDailyFood = queryFactory.selectFrom(QOrderItemDailyFood.orderItemDailyFood)
-                .where(QOrderItemDailyFood.orderItemDailyFood.order.user.id.eq(userId),
-                        QOrderItemDailyFood.orderItemDailyFood.dailyFood.id.eq(dailyFoodId),
-                        QOrderItemDailyFood.orderItemDailyFood.dailyFood.serviceDate.between(LocalDate.now().minusDays(5), LocalDate.now()))
+        return queryFactory.selectFrom(orderItemDailyFood)
+                .where(orderItemDailyFood.order.user.id.eq(userId),
+                        orderItemDailyFood.dailyFood.id.eq(dailyFoodId),
+                        orderItemDailyFood.dailyFood.serviceDate.between(LocalDate.now().minusDays(5), LocalDate.now()))
                 .limit(1)
                 .fetchOne();
-
-        if (orderItemDailyFood != null) System.out.println(orderItemDailyFood.getName());
-
-        return orderItemDailyFood;
-
     }
 
     public List<OrderItemDailyFood> findAllByUserAndPeriod(User user, LocalDate startDate, LocalDate endDate) {
