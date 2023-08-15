9ea7ad2d1bae6daf47ae597bb0f7b4b640e268fc
neya0 <wltjd1484@gmail.com>
2023-08-15 16:11:03 +0900

    백오피스/고객사 주문조회 최적화

---
+++ domain-order/src/main/java/co/dalicious/domain/order/dto/SelectOrderDailyFoodDto.java
@@ -0,0 +1,35 @@
+package co.dalicious.domain.order.dto;
+
+import co.dalicious.system.enums.DiningType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class SelectOrderDailyFoodDto {
+    private BigInteger orderItemGroupId;
+    private LocalDate serviceDate;
+    private DiningType diningType;
+    private String groupName;
+    private String spotName;
+    private String userName;
+    private String userNickname;
+    private String userEmail;
+    private String phone;
+    private String orderCode;
+    private Timestamp orderDateTime;
+    private BigDecimal totalPrice;
+    private BigDecimal supportPrice;
+    private BigDecimal point;
+    private BigDecimal payPrice;
+    private BigDecimal deliveryPrice;
+    private Integer isMembership;
+    private BigInteger userId;
+    private List<SelectOrderItemDailyFoodsDto> selectOrderItemDailyFoodsDtos;
+}
