0d31023c25a2641c8a23571303ceabd1d181420b
gangjiseong <jisung.kang@dalicioius.co>
2023-08-24 15:49:26 +0900

    sse subscribe 요청 lock

--- data-redis/src/main/java/co/dalicious/data/redis/pubsub/SseService.java
+++ data-redis/src/main/java/co/dalicious/data/redis/pubsub/SseService.java
@@ -28,6 +28,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,30 +42,23 @@ public class SseService {
     private final RedisMessageListenerContainer redisMessageListenerContainer;
     private final RedisTemplate redisTemplate;
 
+    // 생명주기 동안 락을 걸 사용자 ID를 저장하는 맵
+    private static final ConcurrentHashMap<BigInteger, Long> lockedUsers = new ConcurrentHashMap<>();
+
     public SseEmitter subscribe(BigInteger userId, String lastEventId) {
+
+        // 이미 락이 걸린 사용자인지 확인
+        if (isLocked(userId)) {
+            // 락이 걸린 사용자에 대해 적절한 응답 또는 예외 처리
+            return emitterRepository.findAllStartWithById(String.valueOf(userId)).get(String.valueOf(userId));
+        }
+
         //구독한 유저를 특정하기 위한 id.
         String id = String.valueOf(userId);
 
         //생성한 emitter를 저장한다. emitter는 HTTP/2기준 브라우저 당 100개 만들 수 있다.
         SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
         System.out.println("emitter = " + emitter);
-
-        //기존 emitter 중 완료 되거나 시간이 초과되어 연결이 끊긴 emitter를 삭제한다.
-        emitter.onCompletion(() -> emitterRepository.deleteById(id));
-        emitter.onTimeout(() -> emitterRepository.deleteById(id));
-
-        emitterRepository.save(id, emitter);
-
-        sendToClient(emitter, id, "EventStream Created. [userId=" + userId + "]");
-
-        // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
-        if (!lastEventId.isEmpty()) {
-            Map<String, Object> events = emitterRepository.findAllEventCacheStartWithId(String.valueOf(userId));
-            events.entrySet().stream()
-                    .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-                    .forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
-        }
-
         MessageListener messageListener = (message, pattern) -> {
             try {
                 System.out.println("Received message from Redis on pattern: " + pattern);
@@ -76,7 +70,23 @@ public class SseService {
 
         redisMessageListenerContainer.addMessageListener(messageListener, ChannelTopic.of(getChannelName(id)));
 
-        checkEmitterStatus(emitter, messageListener);
+        //기존 emitter 중 완료 되거나 시간이 초과되어 연결이 끊긴 emitter를 삭제한다.
+        checkEmitterStatus(emitter, id, messageListener);
+
+        emitterRepository.save(id, emitter);
+
+        sendToClient(emitter, id, "EventStream Created. [userId=" + userId + "]");
+
+        // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
+        if (!lastEventId.isEmpty()) {
+            Map<String, Object> events = emitterRepository.findAllEventCacheStartWithId(String.valueOf(userId));
+            events.entrySet().stream()
+                    .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
+                    .forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
+            emitterRepository.deleteAllEventCacheStartWithId(String.valueOf(userId));
+        }
+
+        setLock(userId);
 
         return emitter;
     }
@@ -109,7 +119,7 @@ public class SseService {
 
     //client에게 이벤트 보내기
     private void sendToClient(SseEmitter emitter, String id, Object data) {
-        System.out.println("emitter = " + emitter);
+        System.out.println("data.toString() = " + data.toString());
         try {
             emitter.send(SseEmitter.event()
                     .id(id)
@@ -165,12 +175,38 @@ public class SseService {
         return sseResponseDtos;
     }
 
-    private void checkEmitterStatus(final SseEmitter emitter, final MessageListener messageListener) {
-        emitter.onCompletion(() -> redisMessageListenerContainer.removeMessageListener(messageListener));
-        emitter.onTimeout(() -> redisMessageListenerContainer.removeMessageListener(messageListener));
+    private void checkEmitterStatus(final SseEmitter emitter,final String id, final MessageListener messageListener) {
+        emitter.onCompletion(() -> {
+            emitterRepository.deleteById(id);
+            redisMessageListenerContainer.removeMessageListener(messageListener);
+        });
+        emitter.onTimeout(() -> {
+            emitterRepository.deleteById(id);
+            redisMessageListenerContainer.removeMessageListener(messageListener);
+        });
     }
 
     private String getChannelName(final String userId) {
         return "appTopics:" + userId;
+    }
+
+    private boolean isLocked(BigInteger userId) {
+        Long lockedTime = lockedUsers.get(userId);
+        if (lockedTime == null) {
+            return false;
+        }
+
+        long elapsedTime = System.currentTimeMillis() - lockedTime;
+        if (elapsedTime > DEFAULT_TIMEOUT) {
+            lockedUsers.remove(userId);
+            return false;
+        }
+
+        return true;
+    }
+
+    // 사용자에게 락을 설정하는 메서드
+    private void setLock(BigInteger userId) {
+        lockedUsers.put(userId, System.currentTimeMillis());
     }
 }
